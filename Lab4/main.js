53c8fce6727ca14f42af7f60897570267505a640
Your Name <you@example.com>
2025-04-19 17:48:10 +0300

    Lab4

---
+++ Lab4/main.js
@@ -0,0 +1,95 @@
+import dishes from './menu.js';
+
+let totalCost = 0;
+const selectedDishes = {
+    soup: null,
+    main: null,
+    drink: null
+};
+
+function addToOrder(keyword) {
+    const dish = dishes.find(d => d.keyword === keyword);
+    if (!dish) return;
+
+    const categoryKey = dish.category;
+
+    document.getElementById('nothing').style.display = 'none';
+    document.getElementById('selected-dishes').style.display = 'block';
+
+    let infoContainer = document.getElementById(`${categoryKey}-info`);
+
+    // Убираем стоимость предыдущего выбранного блюда
+    if (selectedDishes[categoryKey]) {
+        const previousDish = 
+        dishes.find(d => d.keyword === selectedDishes[categoryKey]);
+        totalCost -= previousDish.price;
+
+        // Убираем подсветку с предыдущей карточки
+        const prevCard = 
+        document.querySelector(`[data-dish="${selectedDishes[categoryKey]}"]`);
+        if (prevCard) prevCard.classList.remove('selected');
+    }
+
+    // Обновляем выбранное блюдо
+    selectedDishes[categoryKey] = dish.keyword;
+
+    // Обновляем информацию в блоке
+    infoContainer.innerHTML = `
+        ${categoryKey === 'soup' ? 'Суп' : 
+        categoryKey === 'main' ? 'Основное блюдо' : 'Напиток'}:<br>
+        ${dish.name} - ${dish.price}&#8381;
+    `;
+
+    const hiddenFieldId = `selected-${categoryKey}`;
+    document.getElementById(hiddenFieldId).value = dish.keyword;
+
+    // Подсветка текущей карточки
+    const currentCard = document.querySelector(`[data-dish="${dish.keyword}"]`);
+    if (currentCard) currentCard.classList.add('selected');
+
+    // Прибавляем стоимость
+    totalCost += dish.price;
+
+    // Отображаем итог
+    document.getElementById('total-cost').style.display = 'block';
+    document.getElementById('cost-value').textContent = totalCost;
+}
+
+function displayDishes() {
+    const sections = {
+        soup: document.querySelector('.soup .grid-container'),
+        main: document.querySelector('.main-dishes .grid-container'),
+        drink: document.querySelector('.drink .grid-container')
+    };
+
+    Object.values(sections).forEach(section => section.innerHTML = '');
+
+    const sortedDishes = dishes.sort((a, b) => a.name.localeCompare(b.name));
+
+    sortedDishes.forEach(dish => {
+        const card = document.createElement('div');
+        card.className = 'dish';
+        card.setAttribute('data-dish', dish.keyword);
+        card.innerHTML = `
+            <div class="dish-image">
+                <img src="${dish.image}" alt="${dish.name}">
+            </div>
+            <div class="dish-info">
+                <div class="dish-name-and-price">
+                    <p class="price">${dish.price}&#8381;</p>
+                    <p class="dish-name">${dish.name}</p>
+                </div>
+                    <p class="size">${dish.count}</p>
+                    <button class="dish-button">Добавить</button>
+                </div>
+            </div>
+        `;
+        const button = card.querySelector('.dish-button');
+        button.addEventListener('click', () => addToOrder(dish.keyword));
+        sections[dish.category].appendChild(card);
+    });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    displayDishes();
+});
---
+++ Lab4/menu.js
@@ -0,0 +1,84 @@
+const dishes = [
+    {
+        keyword: "gazpacho",
+        name: "Гаспачо",
+        price: 195,
+        category: "soup",
+        count: "350 г",
+        image: "images/Gazpacho.jpg"
+    },
+    {
+        keyword: "mushroom",
+        name: "Грибной суп",
+        price: 185,
+        category: "soup",
+        count: "330 г",
+        image: "images/mushroom_soup_puree.jpg"
+    },
+    {
+        keyword: "norwegian",
+        name: "Норвежский суп",
+        price: 270,
+        category: "soup",
+        count: "270 г",
+        image: "images/Norwegian-soup.jpg"
+    },
+    {
+        keyword: "tom_yam",
+        name: "Том ям с креветками",
+        price: 365,
+        category: "soup",
+        count: "330 г",
+        image: "images/Tom_yam_with_shrimp.jpg"
+    },
+    {
+        keyword: "lasagna",
+        name: "Лазанья",
+        price: 385,
+        category: "main",
+        count: "340 г",
+        image: "images/lasagna.jpg"
+    },
+    {
+        keyword: "fried_potatoes",
+        name: "Жареная картошка с грибами",
+        price: 150,
+        category: "main",
+        count: "250 г",
+        image: "images/fried_potatoes_with_mushrooms.jpg"
+    },
+    {
+        keyword: "cutlets",
+        name: "Котлеты из курицы с картофельным пюре",
+        price: 225,
+        category: "main",
+        count: "280 г",
+        image: "images/Chicken_cutlets_and_mashed_potatoes.jpg"
+    },
+    {
+        keyword: "orange",
+        name: "Апельсиновый сок",
+        price: 120,
+        category: "drink",
+        count: "300 г",
+        image: "images/Orange_juice.jpg"
+    },
+    {
+        keyword: "apple",
+        name: "Яблочный сок",
+        price: 90,
+        category: "drink",
+        count: "300 г",
+        image: "images/Apple_juice.jpg"
+    },
+    {
+        keyword: "carrot",
+        name: "Морковный сок",
+        price: 110,
+        category: "drink",
+        count: "300 г",
+        image: "images/Carrot_juice.jpg"
+    }
+];
+
+export default dishes;
